aa3d70e3ec6136b311650bc391cea6b4bf768f14
Antonina Pasichnyk <110766517+Tonja1@users.noreply.github.com>
2023-06-14 22:46:43 +0300

    and

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,10 +1,14 @@
-const contactsOperations = require('../../models/contactsModels');
+const contactsOperations = require("../../models/contactsModels");
 
 const getAll = async (req, res, next) => {
+    console.log("TEST getAll");
     try {
         const result = await contactsOperations.listContacts();
+
+        console.log("TEST result", result);
+
         res.json({
-            status: 'success',
+            status: "success",
             code: 200,
             data: {
                 result,
