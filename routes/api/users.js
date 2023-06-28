e48304f5cbfbf179659e9a225e9bf479cc81978f
Antonina Pasichnyk <110766517+Tonja1@users.noreply.github.com>
2023-06-29 00:56:19 +0300

    finishHW4

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { user: ctrl } = require('../../controllers');
+const { validation, authentic } = require('../../middlewares');
+const { schemas } = require('../../models/user');
+const ctrlWrapper = require('../../helpers/ctrlWrapper');
+
+router.get('/current', authentic, ctrlWrapper(ctrl.getCurrent));
+
+router.patch('/', authentic, validation(schemas.subscriptionSchema), ctrlWrapper(ctrl.updateSubscription));
+
+module.exports = router;
